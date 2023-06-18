bbd6acc3f7fca6d17fa044b6d44a39c38511034b
Ashish Juyal <ashish@thecodecamp.in>
2023-06-18 14:49:49 +0530

    empty project structure

---
+++ labs/xp-lab-1/src/test/java/com.test/tdd/StringReverserTest.java
@@ -0,0 +1,14 @@
+package com.test.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringReverserTest {
+
+    @Test public void
+    should_test() {
+
+        assertTrue(true);
+    }
+}
