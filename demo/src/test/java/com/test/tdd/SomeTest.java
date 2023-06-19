9b8fb3584903fd569ba9a26e6cb6c779ab5f3973
Ashish Juyal <ashish@thecodecamp.in>
2023-06-19 23:06:43 +0530

    demo

---
+++ demo/src/test/java/com/test/tdd/SomeTest.java
@@ -0,0 +1,14 @@
+package com.test.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SomeTest {
+
+    @Test public void
+    should_test() {
+
+        assertTrue(true);
+    }
+}
