ec8f9e66abdf6dc1ba428c916275740f8b2e46d9
Ashish Juyal <ashish@thecodecamp.in>
2023-06-20 09:34:00 +0530

    refactoring

--- demo/src/test/java/com/test/tdd/StringReverserDemoTest.java
+++ demo/src/test/java/com/test/tdd/StringReverserDemoTest.java
@@ -1,10 +1,19 @@
 package com.test.tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class StringReverserDemoTest {
+
+   private StringReverser reverser;
+
+   @Before
+   public void setup() {
+     // Arrangement
+     reverser = new StringReverser();
+   }
 
   // TODO:
   // DONE reverseString("") should return ""
@@ -16,15 +25,15 @@ public class StringReverserDemoTest {
 
   @Test public void
   should_return_empty_string_when_input_is_empty() {
-    StringReverser reverser = new StringReverser();
     String str = reverser.reverseString("");
     assertEquals("", str);
   }
 
   @Test public void
   should_return_the_same_character_when_length_of_string_is_one() {
-    StringReverser reverser = new StringReverser();
-    String str = reverser.reverseString("a");
-    assertEquals("a", str);
+    // AAA format
+    String str = reverser.reverseString("a");         // Act
+
+    assertEquals("a", str);                   //Assert
   }
 }
