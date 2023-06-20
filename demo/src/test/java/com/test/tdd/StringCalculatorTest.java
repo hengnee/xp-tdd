61b672857933c777993cc6501475c1f16d4ccb65
Ashish Juyal <ashish@thecodecamp.in>
2023-06-20 14:41:30 +0530

    failing test

--- demo/src/test/java/com/test/tdd/StringCalculatorTest.java
+++ demo/src/test/java/com/test/tdd/StringCalculatorTest.java
@@ -1,7 +1,9 @@
 package com.test.tdd;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,9 +14,11 @@ public class StringCalculatorTest {
   // DONE it can take any amount of numbers and returns the sum.
   // DONE The numbers are delimited by comma "," for e.g “1,2,3”
   // DONE and/or new line character "\n”,  and “1\n2,3” are valid examples.
+  // DONE should throw an exception if there is any negative number,
+  // DONE if there are many negative numbers, show them all in the exception message.
 
-  // should throw an exception if there is any negative number,
-  // if there are many negative numbers, show them all in the exception message.
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
 
   private StringCalculator calc;
   @Before
@@ -49,6 +53,13 @@ public class StringCalculatorTest {
   @Test(expected = IllegalArgumentException.class) public void
   should_throw_an_exception_when_there_is_a_negative_number_in_list() {
     calc.add("1\n2,-3");
+  }
+
+  @Test public void
+  should_throw_an_exception_along_with_all_negative_numbers_when_there_are_multiple_negative_number_in_list() {
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage("multiple negative numbers found. [-2, -3, -4]");
+    calc.add("1,-2,-3,-4");
   }
 
 
