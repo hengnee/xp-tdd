7cb2f1a84513f13a48db235a0074f2ace4e3d469
Training <training@ip-172-31-37-81.ap-southeast-1.compute.internal>
2023-06-20 05:37:13 +0000

    should return empty string when input is empty - failed test

--- labs/xp-lab-1/src/main/java/com.test/StringReverser.java
+++ labs/xp-lab-1/src/main/java/com.test/StringReverser.java
@@ -1,4 +1,5 @@
 package com.test;
 
 public class StringReverser {
+    // TODO: Implement the logic to reverse the input string
 }
--- labs/xp-lab-1/src/test/java/com.test/tdd/StringReverserTest.java
+++ labs/xp-lab-1/src/test/java/com.test/tdd/StringReverserTest.java
@@ -5,10 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringReverserTest {
+    // TODO:
+    // reverseString("") should return ""
+    // reverseString("a") should return "a"
+    // handle null
+    // reverseString("hello") should return "olleh"
+    // reverseString("1234567890") should return "0987654321"
 
     @Test public void
-    should_test() {
-
-        assertTrue(true);
+    should_return_empty_string_when_input_is_empty() {
+        StringReverser stringReverser = new StringReverser();
+        String reversed = stringReverser.reverseString("");
+        assertEquals("", reversed);
     }
 }
