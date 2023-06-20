bd61930d4683b3fc34bf4b755690cc5df8b49e23
Ashish Juyal <ashish@thecodecamp.in>
2023-06-20 14:11:58 +0530

    passing test

--- demo/src/main/java/com/test/tdd/StringCalculator.java
+++ demo/src/main/java/com/test/tdd/StringCalculator.java
@@ -2,9 +2,18 @@ package com.test.tdd;
 
 public class StringCalculator {
   public int add(String numbers) {
+    if (numbers.length() == 0) {
+      return 0;
+    }
+
     if (numbers.length() == 1) {
       return Integer.parseInt(numbers);
     }
-    return 0;
+    int sum = 0;
+    String[] nums = numbers.split(",");
+    for(String n: nums) {
+      sum = sum + Integer.parseInt(n);
+    }
+    return sum;
   }
 }
