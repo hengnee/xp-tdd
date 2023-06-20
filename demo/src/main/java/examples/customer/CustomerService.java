3854b38355c6340db4b495b8b0e90c46e3dfab8f
Ashish Juyal <ashish@thecodecamp.in>
2023-06-21 00:19:21 +0530

    customers lab

---
+++ demo/src/main/java/examples/customer/CustomerService.java
@@ -0,0 +1,16 @@
+package examples.customer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomerService {
+  private final DbRepository repository = new DbRepository();
+
+  public List<Customer> getCustomersByCity(String city) {
+    return repository.findAll()
+        .stream()
+        .filter(c -> c.getCity().equalsIgnoreCase(city))
+        .collect(Collectors.toList());
+  }
+}
---
+++ demo/src/main/java/examples/customer/CustomerTester.java
@@ -0,0 +1,14 @@
+package examples.customer;
+
+import java.util.List;
+
+public class CustomerTester {
+
+  public static void main(String[] args) {
+    CustomerService service = new CustomerService();
+
+    List<Customer> customers = service.getCustomersByCity("New Delhi");
+
+    System.out.println(customers);
+  }
+}
