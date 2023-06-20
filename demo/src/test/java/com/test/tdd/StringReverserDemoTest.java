619738457488e6ce9dc8ccd80e424122d87d5844
Ashish Juyal <ashish@thecodecamp.in>
2023-06-20 09:26:26 +0530

    String reverser third failing test

--- demo/src/test/java/com/test/tdd/StringReverserDemoTest.java
+++ demo/src/test/java/com/test/tdd/StringReverserDemoTest.java
@@ -7,10 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class StringReverserDemoTest {
 
   // TODO:
-  // reverseString("") should return ""
+  // DONE reverseString("") should return ""
+  // reverseString("a") should return "a"
 
   // reverseString("hello") should return "olleh"
-  // reverseString("a") should return "a"
   // reverseString("1234567890") should return "0987654321"
   // handle null
 
@@ -19,5 +19,12 @@ public class StringReverserDemoTest {
     StringReverser reverser = new StringReverser();
     String str = reverser.reverseString("");
     assertEquals("", str);
+  }
+
+  @Test public void
+  should_return_the_same_character_when_length_of_string_is_one() {
+    StringReverser reverser = new StringReverser();
+    String str = reverser.reverseString("a");
+    assertEquals("a", str);
   }
 }
