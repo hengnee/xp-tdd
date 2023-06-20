b8f40ac12ab6c4509a6341096b31644aee6bc22b
Ashish Juyal <ashish@thecodecamp.in>
2023-06-20 14:27:08 +0530

    passing test

--- demo/src/main/java/com/test/tdd/StringCalculator.java
+++ demo/src/main/java/com/test/tdd/StringCalculator.java
@@ -1,5 +1,7 @@
 package com.test.tdd;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StringCalculator {
@@ -7,8 +9,16 @@ public class StringCalculator {
     if (numbers.length() == 0) {
       return 0;
     }
-
     String[] nums = numbers.split(",|\n");
+
+    // validating the input
+    List<Integer> negative = Stream.of(nums)
+        .map(Integer::parseInt)
+        .filter(n -> n < 0).collect(Collectors.toList());
+    if (negative.size() > 0) {
+      throw new IllegalArgumentException("negative number found");
+    }
+
     return Stream.of(nums)
         .map(Integer::parseInt)
         .reduce(0, Integer::sum);
