c36ef5999834230cf774025d52810849434d426f
Ashish Juyal <ashish@thecodecamp.in>
2023-06-20 14:01:27 +0530

    passing test

--- demo/src/main/java/com/test/tdd/StringCalculator.java
+++ demo/src/main/java/com/test/tdd/StringCalculator.java
@@ -2,6 +2,9 @@ package com.test.tdd;
 
 public class StringCalculator {
   public int add(String numbers) {
+    if (numbers.length() == 1) {
+      return Integer.parseInt(numbers);
+    }
     return 0;
   }
 }
