a054014132333aac1aeab0f41fdbcb89dd693c7b
Training <training@ip-172-31-37-81.ap-southeast-1.compute.internal>
2023-06-20 05:52:52 +0000

    removed test duplications

--- labs/xp-lab-1/src/test/java/com.test/tdd/StringReverserTest.java
+++ labs/xp-lab-1/src/test/java/com.test/tdd/StringReverserTest.java
@@ -1,5 +1,6 @@
 package com.test.tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 import com.test.StringReverser;
 import static org.junit.Assert.*;
@@ -11,18 +12,27 @@ public class StringReverserTest {
     // handle null
     // reverseString("hello") should return "olleh"
     // reverseString("1234567890") should return "0987654321"
+    StringReverser stringReverser;
+    @Before
+    void setup() {
+        stringReverser = new StringReverser();
+    }
 
     @Test public void
     should_return_empty_string_when_input_is_empty() {
-        StringReverser stringReverser = new StringReverser();
         String reversed = stringReverser.reverseString("");
         assertEquals("", reversed);
     }
 
     @Test public void
     should_return_same_character_when_input_is_single_character() {
-        StringReverser stringReverser = new StringReverser();
         String reversed = stringReverser.reverseString("a");
         assertEquals("a", reversed);
+    }
+
+    @Test public void
+    should_return_null_when_input_is_null() {
+        String reversed = stringReverser.reverseString(null);
+        assertEquals(null, reversed);
     }
 }
