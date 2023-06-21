1936dde21ceaabab9e6d3353f9411c1193f0b4d6
Ashish Juyal <ashish@thecodecamp.in>
2023-06-21 13:33:44 +0530

    failing test

--- demo/src/test/java/com/test/tdd/NumberGuesserTest.java
+++ demo/src/test/java/com/test/tdd/NumberGuesserTest.java
@@ -2,15 +2,22 @@ package com.test.tdd;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class NumberGuesserTest {
 
-  @Test
-  public void
+  @Test public void
   should_be_able_to_tell_if_the_number_is_higher() {
     NumberGuesser guesser = new NumberGuesser();
     boolean isHigh = guesser.makeGuess(10, 11);
     assertTrue(isHigh);
+  }
+
+  @Test public void
+  should_be_able_to_tell_if_the_number_is_lower() {
+    NumberGuesser guesser = new NumberGuesser();
+    boolean isHigh = guesser.makeGuess(10, 9);
+    assertFalse(isHigh);
   }
 }
