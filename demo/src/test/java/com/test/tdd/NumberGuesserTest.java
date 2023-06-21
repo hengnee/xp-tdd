f712ec8279e938a801f32eced105f83bd5a974c8
Ashish Juyal <ashish@thecodecamp.in>
2023-06-21 13:35:13 +0530

    refactoring

--- demo/src/test/java/com/test/tdd/NumberGuesserTest.java
+++ demo/src/test/java/com/test/tdd/NumberGuesserTest.java
@@ -1,5 +1,6 @@
 package com.test.tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -7,16 +8,20 @@ import static org.junit.Assert.assertTrue;
 
 public class NumberGuesserTest {
 
+  NumberGuesser guesser;
+  @Before
+  public void setup() {
+    guesser = new NumberGuesser();
+  }
+
   @Test public void
   should_be_able_to_tell_if_the_number_is_higher() {
-    NumberGuesser guesser = new NumberGuesser();
     boolean isHigh = guesser.makeGuess(10, 11);
     assertTrue(isHigh);
   }
 
   @Test public void
   should_be_able_to_tell_if_the_number_is_lower() {
-    NumberGuesser guesser = new NumberGuesser();
     boolean isHigh = guesser.makeGuess(10, 9);
     assertFalse(isHigh);
   }
