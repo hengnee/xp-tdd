59cdc1576683e5c4bcd409891dc887b594426887
Ashish Juyal <ashish@thecodecamp.in>
2023-06-22 00:30:49 +0530

    lab-4 in demo

---
+++ demo/src/main/java/refactoring/naming/Time.java
@@ -0,0 +1,13 @@
+package refactoring.naming;
+
+class Time {
+  private final int hour;
+
+  public Time(int hour) {
+    this.hour = hour;
+  }
+
+  public int getHour() {
+    return hour;
+  }
+}
