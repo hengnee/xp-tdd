002d06ebf96235a53c4e20eccb33ba603d2e2c5d
Training <training@ip-172-31-37-81.ap-southeast-1.compute.internal>
2023-06-22 08:17:46 +0000

    Rating refactoring with tests passed

--- labs/tdd-lab-4/src/main/java/refactoring/functions/Rating.java
+++ labs/tdd-lab-4/src/main/java/refactoring/functions/Rating.java
@@ -11,25 +11,23 @@ public class Rating {
   }
 
   public boolean isValidRating() {
-    if (this.getRating() != null) {
-      if (this.getRating().substring(0, 1).equalsIgnoreCase("B") &&
-          this.getRating().length() == 2) {
-        if (StringUtils.isNumeric(this.getRating().substring(1, 2))
-            && Integer.parseInt(this.getRating().substring(1, 2)) > 0
-            && Integer.parseInt(this.getRating().substring(1, 2)) < 5) {
-          return true;
-        }
-      } else if (this.getRating().substring(0, 1).equalsIgnoreCase("A")
-          && this.getRating().length() == 3
-          && StringUtils.isNumeric(this.getRating().substring(1, 3))) {
-        return true;
-      }
-    }
-    return false;
+    return this.getRating() == null ||
+          (isValidRating("A", 3)) ||
+          (isValidRating("B", 2) && isBRatingWithinRange(5));
   }
 
   private String getRating() {
     return rating;
   }
 
+  private boolean isBRatingWithinRange(int max) {
+    int range = Integer.parseInt(this.getRating().substring(1, 2));
+    return  range > 0 && range < max;
+  }
+
+  private boolean isValidRating(String prefix, int limit) {
+    return this.getRating().substring(0, 1).equalsIgnoreCase(prefix) &&
+            this.getRating().length() == limit &&
+            StringUtils.isNumeric(this.getRating().substring(1, limit));
+  }
 }
