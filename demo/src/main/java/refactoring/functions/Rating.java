71e4b7cf5f3f50049c5d3c6260685aee51d181e7
Ashish Juyal <ashish@thecodecamp.in>
2023-06-22 13:43:38 +0530

    extract functions

--- demo/src/main/java/refactoring/functions/Rating.java
+++ demo/src/main/java/refactoring/functions/Rating.java
@@ -2,6 +2,9 @@ package refactoring.functions;
 
 import org.apache.commons.lang3.StringUtils;
 
+import static java.lang.Integer.parseInt;
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 public class Rating {
 
   private final String rating;
@@ -11,21 +14,31 @@ public class Rating {
   }
 
   public boolean isValidRating() {
-    if (this.getRating() != null) {
-      if (this.getRating().substring(0, 1).equalsIgnoreCase("B") &&
-          this.getRating().length() == 2) {
-        if (StringUtils.isNumeric(this.getRating().substring(1, 2))
-            && Integer.parseInt(this.getRating().substring(1, 2)) > 0
-            && Integer.parseInt(this.getRating().substring(1, 2)) < 5) {
-          return true;
-        }
-      } else if (this.getRating().substring(0, 1).equalsIgnoreCase("A")
-          && this.getRating().length() == 3
-          && StringUtils.isNumeric(this.getRating().substring(1, 3))) {
-        return true;
-      }
+    // guard clause
+    if (this.getRating() == null) {
+      return false;
     }
-    return false;
+    if (isRatingB() && isValid()) {
+      return true;
+    }
+    return isRatingA();
+  }
+
+  private boolean isValid() {
+    return isNumeric(this.getRating().substring(1, 2))
+        && parseInt(this.getRating().substring(1, 2)) > 0
+        && parseInt(this.getRating().substring(1, 2)) < 5;
+  }
+
+  private boolean isRatingA() {
+    return this.getRating().substring(0, 1).equalsIgnoreCase("A")
+        && this.getRating().length() == 3
+        && isNumeric(this.getRating().substring(1, 3));
+  }
+
+  private boolean isRatingB() {
+    return this.getRating().substring(0, 1).equalsIgnoreCase("B") &&
+        this.getRating().length() == 2;
   }
 
   private String getRating() {
