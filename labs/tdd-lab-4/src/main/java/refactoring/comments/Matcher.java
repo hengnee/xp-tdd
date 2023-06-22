0fbeee395010fd2dbf965c726203bfa5cbd11f1d
Ashish Juyal <ashish@thecodecamp.in>
2023-06-22 12:38:27 +0530

    comments

--- labs/tdd-lab-4/src/main/java/refactoring/comments/Matcher.java
+++ labs/tdd-lab-4/src/main/java/refactoring/comments/Matcher.java
@@ -5,18 +5,30 @@ public class Matcher {
 
   public boolean match(int[] expected, int[] actual,
                        int clipLimit, int delta) {
-// Clip "too-large" values
-    for (int i1 = 0; i1 < actual.length; i1++)
-      if (actual[i1] > clipLimit)
-        actual[i1] = clipLimit;
-// Check for length differences
-    if (actual.length != expected.length)
+
+    clipTooLargeValues(actual, clipLimit);
+
+    if (isLengthSame(expected, actual))
       return false;
-// Check that each entry is within expected +/- delta
+
+    return isWitinExpectedDelta(expected, actual, delta);
+  }
+
+  private static boolean isLengthSame(int[] expected, int[] actual) {
+    return actual.length != expected.length;
+  }
+
+  private static boolean isWitinExpectedDelta(int[] expected, int[] actual, int delta) {
     for (int i = 0; i < actual.length; i++)
       if (Math.abs(expected[i] - actual[i]) > delta)
         return false;
     return true;
+  }
+
+  private static void clipTooLargeValues(int[] actual, int clipLimit) {
+    for (int i1 = 0; i1 < actual.length; i1++)
+      if (actual[i1] > clipLimit)
+        actual[i1] = clipLimit;
   }
 
 }
