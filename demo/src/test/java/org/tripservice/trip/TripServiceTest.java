6ba1107f05b0f75400891828a06b7db94bff070a
Ashish Juyal <ashish@thecodecamp.in>
2023-06-22 14:46:59 +0530

    trips of friends should return trips

--- demo/src/test/java/org/tripservice/trip/TripServiceTest.java
+++ demo/src/test/java/org/tripservice/trip/TripServiceTest.java
@@ -14,14 +14,18 @@ public class TripServiceTest {
   public static final User SOME_USER = null;
   private static final User GUEST = null;
   private static final User REGISTERED_USER = new User();
-  private static final User ANOTHER_USER = new User();
+  private static User ANOTHER_USER = new User();
   private static final Trip TO_SINGAPORE = new Trip();
+  private static final Trip TO_INDONESIA = new Trip();
   private User loggedInUser;
   TripService tripService;
   @Before
   public void setup() {
-      tripService = new TestableTripService();
+    tripService = new TestableTripService();
+    loggedInUser = REGISTERED_USER;
+    ANOTHER_USER = new User();
   }
+
   @Test(expected = UserNotLoggedInException.class) public void
   should_throw_an_exception_when_the_user_is_not_logged_in() {
     loggedInUser = GUEST;
@@ -30,7 +34,6 @@ public class TripServiceTest {
 
   @Test public void
   should_not_return_any_trips_when_users_are_not_friends() {
-    loggedInUser = REGISTERED_USER;
 
     User notFriend = ANOTHER_USER;
     notFriend.addTrip(TO_SINGAPORE);
@@ -39,10 +42,26 @@ public class TripServiceTest {
     assertEquals(0, trips.size());
   }
 
+  @Test public void
+  should_return_friend_trips_when_user_are_friends() {
+    User friend = ANOTHER_USER;
+    friend.addFriend(loggedInUser);
+    friend.addTrip(TO_SINGAPORE);
+    friend.addTrip(TO_INDONESIA);
+
+    List<Trip> trips = tripService.getTripsByUser(friend);
+    assertEquals(2, trips.size());
+
+  }
   private class TestableTripService extends TripService {
     @Override
     protected User getLoggedInUser() {
       return loggedInUser;
+    }
+
+    @Override
+    protected List<Trip> getTripsBy(User user) {
+      return user.trips();
     }
   }
 }
