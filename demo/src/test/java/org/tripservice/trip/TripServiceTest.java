484e0fe499657ce846ae3c0c5144c3cf82f9a9cd
Ashish Juyal <ashish@thecodecamp.in>
2023-06-22 14:20:00 +0530

    1. test for user not logged in 2. created a seam in TripService

--- demo/src/test/java/org/tripservice/trip/TripServiceTest.java
+++ demo/src/test/java/org/tripservice/trip/TripServiceTest.java
@@ -1,5 +1,26 @@
 package org.tripservice.trip;
 
+import org.junit.Test;
+import org.tripservice.exception.UserNotLoggedInException;
+import org.tripservice.user.User;
+
 public class TripServiceTest {
 
+  public static final User SOME_USER = null;
+  private static final User GUEST = null;
+
+  private User loggedInUser;
+  @Test(expected = UserNotLoggedInException.class) public void
+  should_throw_an_exception_when_the_user_is_not_logged_in() {
+    TripService tripService = new TestableTripService();
+    loggedInUser = GUEST;
+    tripService.getTripsByUser(SOME_USER);
+  }
+
+  private class TestableTripService extends TripService {
+    @Override
+    protected User getLoggedInUser() {
+      return loggedInUser;
+    }
+  }
 }
